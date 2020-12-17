080129fc83364a1e3de66b005440e1f5e3aaa65b
Miguel Gamboa <miguelgamboa@outlook.com>
2020-12-17 20:55:01 +0000

    Refactor routes api with named functions. Add sitemap. Move Router of web api to path /api

--- aula36-vinyl-web-app/lib/routes/routes-vinyl-api.js
+++ aula36-vinyl-web-app/lib/routes/routes-vinyl-api.js
@@ -1,22 +1,27 @@
 'use strict'
 
-const vinyl = require('./../repo/vinyl')
-const users = require('./../repo/users').init()
+const vinyl = require('../repo/vinyl')
+const users = require('../repo/users').init()
 const Router = require('express').Router
 const router = Router()
 
 module.exports = router
 
-router.get('/vinyl/users/:username/toptracks', (req, resp, next) => {
+router.get('/vinyl/users/:username/toptracks', handlerTopTracks)
+router.get('/vinyl/users/:username', handlerUserDetails)
+router.put('/vinyl/users/:username', handlerUserDetailsUpdate)
+router.get('/vinyl/users', handlerUsersList)
+
+function handlerTopTracks (req, resp, next) {
     const limit = req.query.limit | 3
     const username = req.params.username
     vinyl
         .getTopTracks(username, limit)
         .then(tracks => resp.json(tracks))
         .catch(next)
-})
+}
 
-router.get('/vinyl/users/:username', (req, resp, next) => {
+function handlerUserDetails (req, resp, next) {
     const username = req.params.username
     users
         .getUser(username)
@@ -29,9 +34,13 @@ router.get('/vinyl/users/:username', (req, resp, next) => {
             resp.json(user)
         })
         .catch(next)
-})
+}
 
-router.get('/vinyl/users', (req, resp, next) => {
+function handlerUserDetailsUpdate (req, resp, next) {
+    next()
+}
+
+function handlerUsersList (req, resp, next) {
     const host = req.headers.host
     users
         .getUsers()
@@ -41,4 +50,4 @@ router.get('/vinyl/users', (req, resp, next) => {
             return user
         })))
         .catch(next)
-})
+}
--- aula36-vinyl-web-app/lib/server.js
+++ aula36-vinyl-web-app/lib/server.js
@@ -1,20 +1,31 @@
 'use strict'
 
 const express = require('express')
-const routes = require('./routes/routes-vinyl')
+const routesApi = require('./routes/routes-vinyl-api')
+const sitemap = require('express-sitemap-html')
 let server
 
 function init(usersPath, done) {
     if(usersPath)
         require('./repo/users').init(usersPath)
     const app = express()
-    app.use(routes)
+    /**
+     * ROUTES
+     */
+    app.use('/api', routesApi)
+    app.get('/sitemap', sitemap(app))
+    /**
+     * ERROR HAndler
+     */
     app.use((err, req, resp, next) => {
         if(err.status) resp.status(err.status)
         else (resp.status(500))
         resp.send(JSON.stringify(err, Object.getOwnPropertyNames(err)))
         console.log(err)
     })
+    /**
+     * RUN
+     */
     server = app.listen(8000, () => {
         console.log('Listening for HTTP requests on port 8000')
         if (done) done()
